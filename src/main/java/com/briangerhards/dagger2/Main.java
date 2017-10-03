a132db0a20e78a1568d05d78193c98ca23b20a06
Brian Gerhards <brian.gerhards@workiva.com>
2017-10-03 11:08:15 -0500

    Initial push

---
+++ src/main/java/com/briangerhards/dagger2/Main.java
@@ -0,0 +1,16 @@
+package com.briangerhards.dagger2;
+
+//import javax.inject.Inject;
+
+public class Main {
+//    @Inject
+//    NumberOutputService mNumberOutputService;
+//    @Inject
+//    SharedPreferences sharedPreferences;
+
+    public static void main(String[] args) {
+//        InjectorClass.inject(this);
+        System.out.println("Hello world"
+        );
+    }
+}
